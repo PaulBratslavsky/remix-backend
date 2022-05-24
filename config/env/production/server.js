fc599e1cbf5da164a231c62a36f58582527d5626
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.home>
2022-05-24 19:05:41 -0400

    jesus

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => {
-  console.log(env, `###############  env #####################`);
+  console.log(env, `###############  env #####################`, env("RENDER_EXTERNAL_URL"));
   return {
     url: env("RENDER_EXTERNAL_URL"),
   }
