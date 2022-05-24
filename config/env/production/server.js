24761ec71d1ebf17e5ff9ef353310f884c5514f5
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.home>
2022-05-24 17:34:28 -0400

    fixed

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,5 @@
 module.exports = ({ env }) => ({
+  host: env('STRAPI_URL'),
   url: env("RENDER_EXTERNAL_URL"),
 });
+
--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('STRAPI_URL', '0.0.0.0'),
+  host: env('STRAPI_URL'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS')
